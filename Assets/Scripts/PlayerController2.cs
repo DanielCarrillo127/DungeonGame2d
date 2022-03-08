f0d6b2e0b267b9d36afdb45f2ee8b50f8c55679e
DanielRoncallo <roncallodaniel@hotmail.com>
2022-03-07 21:55:38 -0500

    mergepls

---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float runSpeed = 40f;
+    float horizontalMove =0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed",Mathf.Abs(horizontalMove));
+
+
+        if(Input.GetButtonDown("Jump")){
+            jump = true;
+            animator.SetBool("IsJumping",true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch")){
+            crouch = false;
+        }
+
+    }
+    public void OnLanding (){
+        animator.SetBool("IsJumping",false);
+    }
+
+    public void OnCrouching (bool isCrouching){
+        animator.SetBool("IsCrouching",isCrouching);
+    }
+    
+
+     void FixedUpdate() {
+        // Move Character
+        controller.Move(horizontalMove*Time.fixedDeltaTime,crouch,jump);
+        jump = false;
+    }
+    
+}   
+
